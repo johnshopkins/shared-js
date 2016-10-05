4e052503f07fb88019d2347e30dbdc687b0149fa
Jen Wachter <jenleighkelly@gmail.com>
2016-10-05 14:42:07 -0400

    Included only vendor libs

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-  analytics: require("./lib/analytics"),
-  cookie: require("./lib/cookie"),
-  elementResizer: require("./lib/element-resizer"),
-  modernBrowser: require("./lib/modern-browser")
-
-};
